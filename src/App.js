a6f4d80e059cfb05e3878a768da764adb26054f2
Dương Bảo Hưng <56499659+motcondicontrinh1@users.noreply.github.com>
2020-04-29 01:01:18 +0700

    added router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React from 'react';
-import './App.css';
-
-import HomePage from './pages/homepage/homepage.component';
+import React from "react";
+import "./App.css";
+import HomePage from "./pages/homepage/homepage.component";
+import { Route, Switch } from "react-router-dom";
+const HatsPage = () => {
+  return (
+    <div>
+      <h1>HatsPage</h1>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+      <Route exact path="/" component={HomePage} />
+      <Route path="/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
