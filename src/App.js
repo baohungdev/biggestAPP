b3a4a2cf577cc54223dad1cc100d1e1db9198f83
baohungdev <baohungdev@icloud.com>
2020-04-30 00:20:43 +0700

    update menu-item withRouter HOC, add props linkUrl to go to pages

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React from "react";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import { Route, Switch } from "react-router-dom";
+
 const HatsPage = () => {
+  console.log();
   return (
     <div>
       <h1>HatsPage</h1>
@@ -14,8 +16,8 @@ function App() {
   return (
     <div>
       <Switch>
-      <Route exact path="/" component={HomePage} />
-      <Route path="/hats" component={HatsPage} />
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={HatsPage} />
       </Switch>
     </div>
   );
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -4,10 +4,13 @@ import Directory from "../../components/directory/directory.component";
 
 import "./homepage.styles.scss";
 
-const HomePage = () => (
-  <div className="homepage">
-    <Directory />
-  </div>
-);
+const HomePage = () => {
+  console.log();
+  return (
+    <div className="homepage">
+      <Directory />
+    </div>
+  );
+};
 
 export default HomePage;
