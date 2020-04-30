e681f1d025098881bee2f1263cdfe33e8bf809a3
baohungdev <baohungdev@icloud.com>
2020-04-30 09:17:03 +0700

    add collection on shop page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import React from "react";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 import { Route, Switch } from "react-router-dom";
-
-const HatsPage = () => {
-  console.log();
-  return (
-    <div>
-      <h1>HatsPage</h1>
-    </div>
-  );
-};
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/hats" component={HatsPage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import CollectionPreview from "../../components/collection-preview/collection-preview.components";
+
+import SHOP_DATA from "./shop.data";
+
+class ShopPage extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      collections: SHOP_DATA,
+    };
+  }
+
+  render() {
+    const { collections } = this.state;
+    return (
+      <div>
+        {collections.map(({ id, ...otherCollectionProps }) => (
+          <CollectionPreview key={id} {...otherCollectionProps} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default ShopPage;
