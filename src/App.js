008f9af310b7195cea167975e6879e1234c429fb
baohungdev <baohungdev@icloud.com>
2020-05-04 16:11:45 +0700

    finish styling sigininwGG buttons, add auth for user sign out on auth.signOut(), toggle sign in sign out button using ternary operator

--- src/App.js
+++ src/App.js
@@ -1,21 +1,45 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import SignIn from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import { Route, Switch } from "react-router-dom";
 import Header from "./components/header/header.component";
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/shop" component={ShopPage} />
-        <Route path="/signin" component={SignIn} />
-      </Switch>
-    </div>
-  );
+import { auth } from "./components/firebase/firebase.utils";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      currentUser: null,
+    };
+  }
+
+  unsubcribeFormAuth = null;
+
+  componentDidMount() {
+    this.unsubcribeFormAuth = auth.onAuthStateChanged((user) => {
+      this.setState({ currentUser: user });
+      console.log(this.state);
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubcribeFormAuth();
+  }
+
+  render() {
+    return (
+      <div>
+        <Header currentUser={this.state.currentUser} />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
+          <Route path="/signin" component={SignIn} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 import "./header.styles.scss";
-
-const Header = () => {
+import { auth } from "../firebase/firebase.utils";
+const Header = ({ currentUser }) => {
   return (
     <div className="header">
       <Link to="/" className="logo-container">
@@ -16,9 +16,16 @@ const Header = () => {
         <Link to="/shop" className="option">
           CONTACT
         </Link>
-        <Link to="/signin" className="option">
-          SIGN IN
-        </Link>
+        {
+        currentUser ? 
+          <div className="option" onClick={() => auth.signOut()}>
+            SIGN OUT
+          </div>
+         : 
+          <Link to="/signin" className="option">
+            SIGN IN
+          </Link>
+        }
       </div>
     </div>
   );
