09e9d873eac4b9f5a26240a24f0d02e476224504
baohungdev <baohungdev@icloud.com>
2020-05-19 22:55:49 +0700

    add directory to reducer

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,8 +1,10 @@
 import userReducer from "./user/user.reducer";
 import cartReducer from "./cart/cart.reducer";
 import { combineReducers } from "redux";
+import directoryReducer from "./directory/directory.reducer";
 
 export default combineReducers({
   user: userReducer,
   cart: cartReducer,
+  directory: directoryReducer,
 });
