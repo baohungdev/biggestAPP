84f2172411c27d58f424c1ff2608e05fe075b3a7
baohungdev <baohungdev@icloud.com>
2020-05-19 22:16:34 +0700

    added redux-persist

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import ReactDOM from "react-dom";
+import { PersistGate } from "redux-persist/integration/react";
 
 import "./index.css";
 import App from "./App";
 
 import { Provider } from "react-redux";
-import store from "./redux/store";
+import { store, persistor } from "./redux/store";
 ReactDOM.render(
   <Provider store={store}>
     <Router>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </Router>
   </Provider>,
   document.getElementById("root")
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,24 @@
 import { createStore, applyMiddleware } from "redux";
-
+import { persistStore, persistReducer } from "redux-persist";
 import logger from "redux-logger";
+import storage from "redux-persist/lib/storage";
 
 import rootReducer from "./root-reducer";
 
 const middlewares = [logger];
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["cart"],
+};
 
-export default store;
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// const store = createStore(rootReducer, applyMiddleware(...middlewares));
+
+export let store = createStore(
+  persistedReducer,
+  applyMiddleware(...middlewares)
+);
+export let persistor = persistStore(store);
