f52f73ddfeca7b94f02d792d1df954e88ae02b84
Duong Bao Hung <baohungdev@icloud.com>
2020-06-18 01:53:37 +0700

    deleted unused import modules

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,15 +1,7 @@
 import React, { Component } from "react";
-import CollectionOverView from "../../components/collection-overview/collection-overview.component";
 import { Route } from "react-router-dom";
-import CollectionPage from "../collection/collection.component";
 import { connect } from "react-redux";
 import { fetchCollectionsStartAsync } from "../../redux/shop/shop.actions";
-import WithSpinner from "../../components/with-spinner/with-spinner.component";
-import { createStructuredSelector } from "reselect";
-import {
-  selectIsLoadingCollection,
-  selectCollectionisLoaded,
-} from "../../redux/shop/shop.selector";
 import CollectionOverViewContainer from "../../components/collection-overview/collection-overview.container";
 import CollectionPageContainer from "../collection/collection.container";
 
@@ -20,7 +12,7 @@ class ShopPage extends Component {
   }
 
   render() {
-    const { match, isFetching, isLoaded } = this.props;
+    const { match } = this.props;
     return (
       <div className="shop-page">
         <Route
@@ -37,11 +29,7 @@ class ShopPage extends Component {
   }
 }
 
-const mapStateToProps = createStructuredSelector({
-  isLoaded: selectCollectionisLoaded,
-});
-
 const mapDispatchToProps = (dispatch) => ({
   fetchCollectionsStartAsync: () => dispatch(fetchCollectionsStartAsync()),
 });
-export default connect(mapStateToProps, mapDispatchToProps)(ShopPage);
+export default connect(null, mapDispatchToProps)(ShopPage);
